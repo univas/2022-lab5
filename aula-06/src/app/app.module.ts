e4364800b9839d44a7f620ab9e25186a34cf7fec
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-04-04 19:45:08 -0300

    Set default locale

--- aula-06/src/app/app.module.ts
+++ aula-06/src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CpfPipe } from './cpf.pipe';
+import pt from '@angular/common/locales/pt'
+
+registerLocaleData(pt)
 
 @NgModule({
   declarations: [
@@ -12,7 +16,16 @@ import { CpfPipe } from './cpf.pipe';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt'
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
