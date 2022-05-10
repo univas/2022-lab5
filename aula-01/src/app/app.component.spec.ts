b57d29eeb363b0e83b8f2ebb0bc5f493682bf957
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-05-10 06:21:57 -0300

    Fix unit test aula-01

--- aula-01/src/app/app.component.spec.ts
+++ aula-01/src/app/app.component.spec.ts
@@ -16,16 +16,24 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'aula-01'`, () => {
+  it(`should have as title 'Univas'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('aula-01');
+    expect(app.title).toEqual('Univas');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('aula-01 app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Aprendendo Angular');
   });
+
+  it('should increment the counter instance variable', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.counter).toBe(0)
+    app.incrementCounter()
+    expect(app.counter).toBe(1)
+  })
 });
