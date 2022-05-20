0dfc7c37a25e3cd5c34e918653f92f5935e3791e
Rodrigo Faria <rodrigolfsi@gmail.com>
2022-05-19 22:06:10 -0300

    Complete aula-10

--- aula-10/src/app/app.component.ts
+++ aula-10/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Post } from './post';
 import { PostService } from './post.service';
 
@@ -9,15 +10,34 @@ import { PostService } from './post.service';
 })
 export class AppComponent {
 
-  postList : Post[] = []
+  postList  : Post[] = []
+  newPost   : Post = {} as Post
   projectId = '29fa8892-241b-49a9-be59-b4af497257d0'
 
   constructor(private service: PostService) {}
 
   ngOnInit() {
+    this.loadData()
+  }
+
+  loadData() {
     this.service.getPosts(this.projectId).subscribe(items => {
       this.postList = items
     })
   }
 
+  save(myForm: NgForm) {
+    this.newPost.project_uuid = this.projectId
+    this.service.save(this.newPost, this.projectId).subscribe(() => {
+      myForm.resetForm()
+      this.newPost = {} as Post
+      this.loadData()
+    })
+  }
+
+  delete(postId: number) {
+    this.service.delete(this.projectId, postId).subscribe(() => {
+      this.loadData()
+    })
+  }
 }
--- aula-10/src/app/post.service.ts
+++ aula-10/src/app/post.service.ts
@@ -16,4 +16,11 @@ export class PostService {
     return this.http.get<Post[]>(this.url + projectUUID)
   }
 
+  save(post: Post, projectUUID: string) : Observable<Post> {
+    return this.http.post<Post>(this.url + projectUUID, post)
+  }
+
+  delete(projectUUID: string, postId: number) : Observable<Post> {
+    return this.http.delete<Post>(this.url + projectUUID + "/" + postId)
+  }
 }
